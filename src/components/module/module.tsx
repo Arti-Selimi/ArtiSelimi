4c65c9ecb0f72e77f42c893dcc7d908abe19d237
Arti <selimiarti@outlook.com>
2025-04-03 00:00:43 +0200

    added module component and basic functionality

--- src/components/module/module.tsx
+++ src/components/module/module.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
 type Props = {
-    link: string
-}
+  link: string;
+  onClose: () => void;
+};
 
-export default function Module({link}: Props) {
+export default function Module({ link, onClose }: Props) {
+  const [open, setOpen] = useState(true);
+
   return (
-    <div>
-        <iframe src={link} />
-    </div>
-  )
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="absolute !min-w-[90vw] !min-h-[90vh] p-0 text-white text-3xl">
+        <DialogTitle className="absolute top-5 left-5">
+          Preview Window
+        </DialogTitle>
+        <iframe
+          src={link}
+          className="w-full h-full rounded-lg border border-gray-700"
+        />
+      </DialogContent>
+    </Dialog>
+  );
 }
--- src/components/tiles/tile.tsx
+++ src/components/tiles/tile.tsx
@@ -1,70 +1,56 @@
-import React, { ReactNode } from "react";
+"use client"
+
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { FaArrowRight, FaExpand } from "react-icons/fa";
 import { FiGithub } from "react-icons/fi";
 import type { StaticImageData } from "next/image";
+import Module from "../module/module";
 
 type Props = {
-  name?: string;
-  imgSrc?: string | StaticImageData;
-  frameworks?: string;
-  link?: string;
-  className?: string;
-  content?: ReactNode;
-  contentType?: string;
+  name: string;
+  imgSrc: string | StaticImageData;
+  frameworks: string;
+  link: string;
+  deploymentUrl: string;
 };
 
-export default function Tile({
-  name,
-  imgSrc,
-  frameworks,
-  link,
-  className,
-  content,
-  contentType,
-}: Props) {
+export default function Tile({ name, imgSrc, frameworks, link, deploymentUrl }: Props) {
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <>
-      {contentType === "icon" ? (
-        <div className="p-5 rounded-[15px] bg-black  shadow-xl flex items-center justify-center h-[100px] w-[100px]">
-          {content}
+    <div className="p-5 rounded-[15px] bg-black  shadow-xl flex flex-col justify-center items-start w-full h-full">
+      <div className="border-b-2 border-primary pb-1 px-2 w-full mb-2 flex justify-between items-center">
+        <div>
+          <h1 className="text-2xl font-extrabold">{name}</h1>
         </div>
-      ) : (
-          <div className="p-5 rounded-[15px] bg-black  shadow-xl flex flex-col justify-center items-start w-full h-full">
-            <div className="border-b-2 border-primary pb-1 px-2 w-full mb-2 flex justify-between items-center">
-              <div>
-                <h1 className="text-2xl font-extrabold">{name}</h1>
-              </div>
-              <div className="flex gap-5 items-center">
-                <Link
-                  href={`${link}`}
-                  target="_blank"
-                >
-                  <FiGithub size={25} />
-                </Link>
-                <FaExpand size={25} />
-              </div>
-            </div>
-            {imgSrc && (
-              <Image
-                src={imgSrc}
-                alt="project"
-                width={500}
-                height={300}
-                className="w-full h-full rounded-[15px]"
-              />
-            )}
-            <div className="flex flex-col justify-center items-start w-full h-full">
-              <div className="pt-1 px-2 w-full flex justify-between items-center">
-                <div>
-                  <h1 className="text-xl font-extrabold">{frameworks}</h1>
-                </div>
-                <FaArrowRight size={25} className="-rotate-45" />
-              </div>
-            </div>
-          </div>
+        <div className="flex gap-5 items-center">
+          <Link href={`${link}`} target="_blank">
+            <FiGithub size={25} />
+          </Link>
+          <FaExpand size={25} onClick={() => setIsOpen(true)} />
+        </div>
+      </div>
+      {imgSrc && (
+        <Image
+          src={imgSrc}
+          alt="project"
+          width={500}
+          height={300}
+          className="w-full h-full rounded-[15px]"
+        />
       )}
-    </>
+      <div className="flex flex-col justify-center items-start w-full h-full">
+        <div className="pt-1 px-2 w-full flex justify-between items-center">
+          <div>
+            <h1 className="text-xl font-extrabold">{frameworks}</h1>
+          </div>
+          <FaArrowRight size={25} className="-rotate-45" />
+        </div>
+      </div>
+      {isOpen && <Module link={deploymentUrl} onClose={() => setIsOpen(false)} />}
+    </div>
   );
 }
